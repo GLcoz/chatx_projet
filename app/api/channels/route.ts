5679771182a709892c87ffdf40184a55c7cae12b
hakim fatmi <126871854+GLcoz@users.noreply.github.com>
2024-07-10 19:57:53 +0100

    Update route.ts

--- app/api/channels/route.ts
+++ app/api/channels/route.ts
@@ -4,8 +4,68 @@ import { MemberRole } from "@prisma/client";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 
-export async function POST(
-  req: Request
+export async function DELETE(
+  req: Request,
+  { params }: { params: { channelId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    const { searchParams } = new URL(req.url);
+
+    const serverId = searchParams.get("serverId");
+
+    console.log("Profile:", profile);
+    console.log("Server ID:", serverId);
+    console.log("Channel ID:", params.channelId);
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!serverId) {
+      return new NextResponse("Server ID missing", { status: 400 });
+    }
+
+    if (!params.channelId) {
+      return new NextResponse("Channel ID missing", { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        members: {
+          some: {
+            profileId: profile.id,
+            role: {
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
+            }
+          }
+        }
+      },
+      data: {
+        channels: {
+          delete: {
+            id: params.channelId,
+            name: {
+              not: "general",
+            }
+          }
+        }
+      }
+    });
+
+    console.log("Server after delete:", server);
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[CHANNEL_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { channelId: string } }
 ) {
   try {
     const profile = await currentProfile();
@@ -14,12 +74,22 @@ export async function POST(
 
     const serverId = searchParams.get("serverId");
 
+    console.log("Profile:", profile);
+    console.log("Server ID:", serverId);
+    console.log("Channel ID:", params.channelId);
+    console.log("New Name:", name);
+    console.log("New Type:", type);
+
     if (!profile) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
     if (!serverId) {
       return new NextResponse("Server ID missing", { status: 400 });
+    }
+
+    if (!params.channelId) {
+      return new NextResponse("Channel ID missing", { status: 400 });
     }
 
     if (name === "general") {
@@ -33,25 +103,34 @@ export async function POST(
           some: {
             profileId: profile.id,
             role: {
-              in: [MemberRole.ADMIN, MemberRole.MODERATOR]
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
             }
           }
         }
       },
       data: {
         channels: {
-          create: {
-            profileId: profile.id,
-            name,
-            type,
+          update: {
+            where: {
+              id: params.channelId,
+              NOT: {
+                name: "general",
+              },
+            },
+            data: {
+              name,
+              type,
+            }
           }
         }
       }
     });
 
+    console.log("Server after update:", server);
+
     return NextResponse.json(server);
   } catch (error) {
-    console.log("CHANNELS_POST", error);
+    console.log("[CHANNEL_ID_PATCH]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
