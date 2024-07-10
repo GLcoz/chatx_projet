9faedeb8e7a90341d86b6a2ca89722aea890ac09
GLcoz <hakim.fatmi59@gmail.com>
2024-07-10 21:21:05 +0100

    Fix build issues and update API routes

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-declare global {
-  var prisma: PrismaClient | undefined;
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const db = globalThis.prisma || new PrismaClient();
+export const db =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query', 'info', 'warn', 'error'],
+  });
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
